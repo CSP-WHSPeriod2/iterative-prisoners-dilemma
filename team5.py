c1bbb8236bfe6b412b3a501c7ae32cf5eef7d497
mrblackford <41639748+mrblackford@users.noreply.github.com>
2018-10-18 09:34:23 -0700

    Team 5 upload

--- team5.py
+++ team5.py
@@ -5,10 +5,75 @@
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
+import random
+team_name = 'BreadGetters' # Only 10 chars displayed.
+strategy_name = 'Getting Bread'
+strategy_description = 'gets bread'
+n=-1
+g=[0]
+def alg_test(my_history,their_history,my_score,their_score):
+    if(their_history[-1] == their_history[-2]):
+        if(their_history[-1] == 'b'):
+            return 'b'
+        else:
+            return 'c'
+    elif(their_history[-1] == their_history[-3]):
+        if(their_history[-1] == 'b'):
+            return 'b'
+        else:
+            return 'c'
+    elif(their_history[-1] == their_history[-4]):
+        if(their_history[-1] == 'b'):
+            return 'b'
+        else:
+            return 'c'
+    elif(their_history[-1] == their_history[-5]):
+        if(their_history[-1] == 'b'):
+            return 'b'
+        else:
+            return 'c'
+    elif(their_history[-1] == their_history[-6]):
+        if(their_history[-1] == 'b'):
+            return 'b'
+        else:
+            return 'c'
+    elif(their_history[-1] == their_history[-7]):
+        if(their_history[-1] == 'b'):
+            return 'b'
+        else:
+            return 'c'
+    elif(their_history[-1] == their_history[-8]):
+        if(their_history[-1] == 'b'):
+            return 'b'
+        else:
+            return 'c'
+    elif(their_history[-1] == their_history[-9]):
+        if(their_history[-1] == 'b'):
+            return 'b'
+        else:
+            return 'c'
+    elif(their_history[-1] == their_history[-10]):
+        if(their_history[-1] == 'b'):
+            return 'b'
+        else:
+            return 'c'
+    
+def ygolohcysp(my_history, their_history, my_score, their_score):
+    if (their_history[-1] == 'b'):
+        return 'c'
+    else:
+        return 'b'
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+def bodds(my_history, their_history, my_score, their_score):
+    global n
+    global g
+    n+=1
+    l=g[n]+1
+    g=g+l
+    if random.randint(1,100) in g:
+        return 'b'
+    else:
+        return alg_test(my_history, their_history, my_score, their_score)
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,52 +82,22 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if(len(my_history) <= 10):
+        if(len(my_history == 0)):
+            return 'b'
+        return ygolohcysp(my_history, their_history, my_score, their_score)
+    if(my_score >= their_score):
+        return ygolohcysp(my_history, their_history, my_score, their_score) 
+    elif(len(my_history)>=50):
+        return bodds()
+    else:
+        return alg_test(my_history,their_history, my_score, their_score)
+        
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
     # The most recent round is my_history[-1] and their_history[-1].
     
     # Analyze my_history and their_history and/or my_score and their_score.
-    # Decide whether to return 'c' or 'b'.
-    
+    # Decide whether to return 'c' or 'b'
     return 'c'
-
-    
-def test_move(my_history, their_history, my_score, their_score, result):
-    '''calls move(my_history, their_history, my_score, their_score)
-    from this module. Prints error if return value != result.
-    Returns True or False, dpending on whether result was as expected.
-    '''
-    real_result = move(my_history, their_history, my_score, their_score)
-    if real_result == result:
-        return True
-    else:
-        print("move(" +
-            ", ".join(["'"+my_history+"'", "'"+their_history+"'",
-                       str(my_score), str(their_score)])+
-            ") returned " + "'" + real_result + "'" +
-            " and should have returned '" + result + "'")
-        return False
-
-if __name__ == '__main__':
-     
-    # Test 1: Betray on first move.
-    if test_move(my_history='',
-              their_history='', 
-              my_score=0,
-              their_score=0,
-              result='b'):
-         print 'Test passed'
-     # Test 2: Continue betraying if they collude despite being betrayed.
-    test_move(my_history='bbb',
-              their_history='ccc', 
-              # Note the scores are for testing move().
-              # The history and scores don't need to match unless
-              # that is relevant to the test of move(). Here,
-              # the simulation (if working correctly) would have awarded 
-              # 300 to me and -750 to them. This test will pass if and only if
-              # move('bbb', 'ccc', 0, 0) returns 'b'.
-              my_score=0, 
-              their_score=0,
-              result='b')             
